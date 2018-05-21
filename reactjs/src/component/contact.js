9d3cb63a9af17ce063fe2a73f6f33055c44acfc0
Rice <hieuhanufit@fabbi.com.vn>
2018-05-21 10:49:52 +0700

    Change the contact.js

--- reactjs/src/component/contact.js
+++ reactjs/src/component/contact.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as 
+  Redirect
+} from "react-router-dom";
 
 export default class Contact extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isDirected: false,
+      fname:'',
+      femail:'',
+      fphone:'',
+      fmessage:''
+        }
+    
+  }
+  isChanged = (event) => {
+   const name = event.target.name;
+   const value = event.target.value;
+   this.setState ( {
+     [name]:value
+   })
+  }
+
+  submitForm = (event) => {
+    event.preventDefault();
+    this.setState({
+      isDirected:true,
+    
+    })
     
   }
   render() {
+    if(this.state.isDirected ){
+      console.log(this.state.femail)
+      return <Redirect to="/" />;
+    }
     return (
       <div>
         <div>
@@ -32,35 +63,35 @@ export default class Contact extends Component {
           <div className="control-group">
             <div className="form-group floating-label-form-group controls">
               <label>Name</label>
-              <input type="text" className="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name." />
+              <input type="text" name = "fname" onChange = {(event)=>this.isChanged(event)} className="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name." />
               <p className="help-block text-danger" />
             </div>
           </div>
           <div className="control-group">
             <div className="form-group floating-label-form-group controls">
               <label>Email Address</label>
-              <input type="email" className="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address." />
+              <input type="email" name="femail" onChange = {(event)=>this.isChanged(event)} className="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address." />
               <p className="help-block text-danger" />
             </div>
           </div>
           <div className="control-group">
             <div className="form-group col-xs-12 floating-label-form-group controls">
               <label>Phone Number</label>
-              <input type="tel" className="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number." />
+              <input type="tel" onChange = {(event)=>this.isChanged(event)} name="fphone" className="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number." />
               <p className="help-block text-danger" />
             </div>
           </div>
           <div className="control-group">
             <div className="form-group floating-label-form-group controls">
               <label>Message</label>
-              <textarea rows={5} className="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message." defaultValue={""} />
+              <textarea rows={5}  name = "fmessages" onChange = {(event)=>this.isChanged(event)} className="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message." defaultValue={""} />
               <p className="help-block text-danger" />
             </div>
           </div>
           <br />
           <div id="success" />
           <div className="form-group">
-            <button type="submit" className="btn btn-primary" id="sendMessageButton">Send</button>
+            <button type="submit" onClick={(event)=> {this.submitForm(event)}} className="btn btn-primary" id="sendMessageButton">Send</button>
           </div>
         </form>
       </div>
